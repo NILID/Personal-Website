2b425d49f6af5440617ccc3228861cbbd253f363
Flavio <flavioandrade0201@gmail.com>
2021-04-19 20:27:03 +0300

    Contact Flex and Email Container created - Sass files included and imported

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../styles/Contact.scss";
+import ContactEmailContainer from "./ContactEmailContainer";
 
 function Contact() {
   return (
     <div className="ContactContainer">
       <h1>Let's talk</h1>
+      <ContactEmailContainer></ContactEmailContainer>
     </div>
   );
 }
