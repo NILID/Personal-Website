1c6c196a502af4732242afd731af5acda79fb2c7
Flavio <flavioandrade0201@gmail.com>
2021-04-25 21:08:18 +0300

    Tabs component added

--- src/components/About/AboutMe.js
+++ src/components/About/AboutMe.js
@@ -1,12 +1,27 @@
 import React from "react";
+import flavio from "../../assets/flavio.jpg";
+import Tabs from "./Tabs";
 import "./AboutMe.scss";
 
 function AboutMe() {
   return (
     <div className="about-container">
-      <div className="grid-1 flex-abt">container 1</div>
-      <div className="grid-2 flex-abt">container 2</div>
-      <div className="grid-3 flex-abt">container 3</div>
+      <div className="grid-1 flex-abt">
+        <img src={flavio} alt="" />
+      </div>
+      <div className="grid-2 flex-abt">
+        <h1>About me</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam
+          sunt labore similique ut earum nam nihil voluptas hic quasi id? Libero
+          iste corrupti recusandae! Autem voluptatem aperiam perferendis ut
+          quasi?
+        </p>
+      </div>
+      <div className="grid-3 flex-abt">
+        <Tabs />
+      </div>
+      <div className="grid-4 flex-abt">container 4</div>
     </div>
   );
 }
