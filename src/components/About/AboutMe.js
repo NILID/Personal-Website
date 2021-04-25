96a81d449b8d9b904d58bed043b4601ef6714fd7
drrh12 <flavioandrade0201@gmail.com>
2021-04-25 16:55:05 +0300

    Components deleted to be redone - scss files also

--- src/components/About/AboutMe.js
+++ src/components/About/AboutMe.js
@@ -1,15 +1,7 @@
 import React from "react";
-import "../../styles/AboutMe.scss";
-import AboutMeGrid from "./AboutMeGrid";
 
 function AboutMe() {
-  return (
-    <div className="AboutMeContainer">
-      <h1>Technologies</h1>
-      <h4>Here are some techs and programming languages that I use</h4>
-      <AboutMeGrid></AboutMeGrid>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default AboutMe;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,11 @@
-import Menu from "./Menu/Menu";
 import Home from "./Home/Home";
-import StacksMainComponent from "./About/StacksMainComponent";
-import Projects from "./Projects/Projects";
-import Contact from "./Contact/Contact";
+
 import "../styles/Main.scss";
-import AboutMe from "./About/AboutMe";
 
 function Main() {
   return (
     <div>
       <Home></Home>
-      {/* <AboutMe></AboutMe>
-      <Projects></Projects>
-      <Contact></Contact> */}
     </div>
   );
 }
