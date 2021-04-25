631b7826a4fa0bdfa74088e5a5c5b5c3b45d89d2
Flavio <flavioandrade0201@gmail.com>
2021-04-25 20:26:20 +0300

    About me grid done

--- src/components/About/AboutMe.js
+++ src/components/About/AboutMe.js
@@ -4,9 +4,9 @@ import "./AboutMe.scss";
 function AboutMe() {
   return (
     <div className="about-container">
-      <div>container 1</div>
-      <div>container 2</div>
-      <div>container 3</div>
+      <div className="grid-1 flex-abt">container 1</div>
+      <div className="grid-2 flex-abt">container 2</div>
+      <div className="grid-3 flex-abt">container 3</div>
     </div>
   );
 }
