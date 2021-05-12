6a9c3a77df3760165cae7c87bbb1f5526e83e47f
Flavio <flavioandrade0201@gmail.com>
2021-05-12 21:19:40 +0300

    changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,36 @@ import "./Projects.scss";
 function Projects() {
   return (
     <div className="projects-container">
-      <div className="projects-grid">1</div>
-      <div className="projects-grid">2</div>
-      <div className="projects-grid">3</div>
-      <div className="projects-grid">4</div>
+      <div className="projects-grid">
+        <div className="img">
+          <img
+            className="img2"
+            src="https://dummyimage.com/600x400/000/fff"
+            alt=""
+          />
+        </div>
+        <div className="desc">
+          <h4>AI faces</h4>
+        </div>
+      </div>
+      <div className="projects-grid">
+        <div className="img">1</div>
+        <div className="desc">
+          <h4>AI faces</h4>
+        </div>
+      </div>
+      <div className="projects-grid">
+        <div className="img">1</div>
+        <div className="desc">
+          <h4>AI faces</h4>
+        </div>
+      </div>
+      <div className="projects-grid">
+        <div className="img">1</div>
+        <div className="desc">
+          <h4>AI faces</h4>
+        </div>
+      </div>
     </div>
   );
 }
