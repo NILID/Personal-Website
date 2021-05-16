c99cd48a1d4a7c4ae074720e3235597426924528
Flavio <flavioandrade0201@gmail.com>
2021-05-16 17:19:47 +0300

    filed added

---
+++ src/components/Bottom/Bottom.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Bottom.scss";
+
+function Bottom() {
+  return <div className="bottom-container">Bottom</div>;
+}
+
+export default Bottom;
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Contact.scss";
+
+function Contact() {
+  return (
+    <div className="contact-container">
+      <h1>contact</h1>
+    </div>
+  );
+}
+
+export default Contact;
