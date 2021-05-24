df1cba47927bea2b82eaeb158341295918c54567
drrh12 <flavioandrade0201@gmail.com>
2021-05-24 21:22:42 +0300

    placeholder added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,35 +1,16 @@
 import React from "react";
 import "./Projects.scss";
+import placeholder from "../../assets/placeholder.svg";
 
 function Projects() {
   return (
     <div class="grid-container">
       <div class="grid-item">
-        <img
-          // src="https://cdn.iconscout.com/icon/premium/png-256-thumb/artificial-intelligence-2342301-1956433.png"
-          alt=""
-        />
+        <img src={placeholder} alt="placeholder" />
         <h2>AI built with Javascript</h2>
       </div>
       <div class="grid-item">
-        <img
-          // src="https://cdn.iconscout.com/icon/premium/png-256-thumb/artificial-intelligence-2342301-1956433.png"
-          alt=""
-        />
-        <h2>AI built with Javascript</h2>
-      </div>
-      <div class="grid-item">
-        <img
-          // src="https://cdn.iconscout.com/icon/premium/png-256-thumb/artificial-intelligence-2342301-1956433.png"
-          alt=""
-        />
-        <h2>AI built with Javascript</h2>
-      </div>
-      <div class="grid-item">
-        <img
-          // src="https://cdn.iconscout.com/icon/premium/png-256-thumb/artificial-intelligence-2342301-1956433.png"
-          alt=""
-        />
+        <img src={placeholder} alt="placeholder" />
         <h2>AI built with Javascript</h2>
       </div>
     </div>
