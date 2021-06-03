247253c7205249302effeac1a8a6e1c2bf20ca45
Flavio <flavioandrade0201@gmail.com>
2021-06-03 17:20:00 +0300

    bottom changed, projects changed

--- src/components/Bottom/Bottom.js
+++ src/components/Bottom/Bottom.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./Bottom.scss";
+import facebook from "../../assets/facebook.svg";
+import github from "../../assets/github.svg";
+import linkedin from "../../assets/linkedin.svg";
 
 function Bottom() {
-  return <div className="bottom-container">Bottom</div>;
+  return (
+    <div className="bottom-container">
+      <div className="icons">
+        <img src={facebook} alt="" />
+      </div>
+      <div className="icons">
+        <img src={github} alt="" />
+      </div>
+      <div className="icons">
+        <img src={linkedin} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Bottom;
