541e3d45b104e48d1e74ac88324365a510953b3f
Flavio <flavioandrade0201@gmail.com>
2021-06-22 21:19:04 +0300

    PageNotFound added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import Main from "./components/Main";
 import Home from "./components/Home/Home";
+import About from "./components/About/AboutMe";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
+import PageNotFound from "./PageNotFound";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/" component={Main} />
-        <Route path="/home" component={Home} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/contact" component={Contact} />
+        <Route path="/" exact component={Main} />
+        <Route path="/about" exact component={About} />
+        <Route path="/projects" exact component={Projects} />
+        <Route path="/contact" exact component={Contact} />
+        <Route path="*" exact component={PageNotFound} />
       </Switch>
     </div>
   );
