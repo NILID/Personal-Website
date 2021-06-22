e7438ab7d0a88817da5392db471d89af867dd4a5
Flavio <flavioandrade0201@gmail.com>
2021-06-22 21:10:19 +0300

    react router added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { Switch, Route } from "react-router-dom";
-import Main from "./components/Main"
+import Main from "./components/Main";
+import Home from "./components/Home/Home";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route>
-          <Main exact path="/"></Main>
-        </Route>
+        <Route path="/" component={Main} />
+        <Route path="/home" component={Home} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
       </Switch>
     </div>
   );
