862d5ff3316fbb006813ecebb70d297e972b049c
Flavio <flavioandrade0201@gmail.com>
2021-07-11 07:49:05 +0300

    new markup for tabs

--- src/components/Tabs/Tab.jsx
+++ src/components/Tabs/Tab.jsx
@@ -1,97 +1,48 @@
-import React from "react";
-import PropTypes from "prop-types";
-import SwipeableViews from "react-swipeable-views";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
+import React, { useState } from "react";
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+const Tab = () => {
+  const [ToggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
+  const getActiveClass = (index, className) =>
+    ToggleState === index ? className : "";
 
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `full-width-tab-${index}`,
-    "aria-controls": `full-width-tabpanel-${index}`,
-  };
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.background.paper,
-    width: 500,
-  },
-}));
-
-export default function FullWidthTabs() {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  const handleChangeIndex = (index) => {
-    setValue(index);
-  };
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
-          aria-label="full width tabs example"
+    <div className="container">
+      <ul className="tab-list">
+        <li
+          className={`tabs ${getActiveClass(1, "active-tabs")}`}
+          onClick={() => toggleTab(1)}
         >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-        </Tabs>
-      </AppBar>
-      <SwipeableViews
-        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      >
-        <TabPanel value={value} index={0} dir={theme.direction}>
-          Item One
-        </TabPanel>
-        <TabPanel value={value} index={1} dir={theme.direction}>
-          Item Two
-        </TabPanel>
-        <TabPanel value={value} index={2} dir={theme.direction}>
-          Item Three
-        </TabPanel>
-      </SwipeableViews>
+          Tab 1
+        </li>
+        <li
+          className={`tabs ${getActiveClass(2, "active-tabs")}`}
+          onClick={() => toggleTab(2)}
+        >
+          Tab 2
+        </li>
+        <li
+          className={`tabs ${getActiveClass(3, "active-tabs")}`}
+          onClick={() => toggleTab(3)}
+        >
+          Tab 3
+        </li>
+      </ul>
+      <div className="content-container">
+        <div className={`content ${getActiveClass(1, "active-content")}`}>
+          <h2>Lorem</h2>
+        </div>
+        <div className={`content ${getActiveClass(2, "active-content")}`}>
+          <h2>Ipsum</h2>
+        </div>
+        <div className={`content ${getActiveClass(3, "active-content")}`}>
+          <h2>Dolor</h2>
+        </div>
+      </div>
     </div>
   );
-}
+};
