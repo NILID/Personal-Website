90bf31d102b2a050f28d6ea73d16e9ddabbf7683
Flavio <flavioandrade0201@gmail.com>
2021-07-16 13:54:30 +0300

    languages files added

---
+++ src/components/languages.js
@@ -0,0 +1,12 @@
+const text = {
+  en: {
+    about: "about",
+    project: "project",
+    contact: "contact",
+    slogan: "Think the Design, and I design the Code.",
+    button: "Learn more",
+  },
+  ptbr: {},
+  jp: {},
+  ru: {},
+};
