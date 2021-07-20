ee5f7ac6598047e18eb5144eef6c4cdc952f1962
Flavio <flavioandrade0201@gmail.com>
2021-07-20 06:15:20 +0300

    functions for the new menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Menu() {
-  return <div></div>;
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const close = () => setClick(false);
+
+  return (
+    <>
+      <Container>
+        <MainContainer
+          className={click ? "main-container" : ""}
+          onClick={() => close}
+        ></MainContainer>
+      </Container>
+    </>
+  );
 }
 
 export default Menu;
