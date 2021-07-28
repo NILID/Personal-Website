5bfa44b5ebd0df0660c30fae64cbcf411c1cb078
Flavio <flavioandrade0201@gmail.com>
2021-07-28 12:10:31 +0300

    dummy texts added in dictionary

--- src/components/Dictionary/dictionary.js
+++ src/components/Dictionary/dictionary.js
@@ -1,5 +1,56 @@
 const dictionary = {
-  en: [{}],
+  en: [
+    {
+      about: "about",
+      project: "project",
+      contact: "contact",
+      slogan: "Think the Design",
+      button: "Learn more",
+      titleAboutMe: "About me",
+      messageAboutMe: "I'm myself",
+      titleProjects: "Projects",
+      titleContact: "Contact",
+    },
+  ],
+  ptbr: [
+    {
+      about: "sobre",
+      project: "projetos",
+      contact: "contato",
+      slogan: "Pense no Design e eu vou desenhar teu código",
+      button: "Saiba mais",
+      titleAboutMe: "Sobre",
+      messageAboutMe: "Bonitão",
+      titleProjects: "Projetos",
+      titleContact: "Contatos",
+    },
+  ],
+  jp: [
+    {
+      about: "nihon",
+      project: "nihon",
+      contact: "nihon",
+      slogan: "nihon",
+      button: "nihon",
+      titleAboutMe: "nihon",
+      messageAboutMe: "nihon",
+      titleProjects: "nihon",
+      titleContact: "nihon",
+    },
+  ],
+  ru: [
+    {
+      about: "руский",
+      project: "руский",
+      contact: "руский",
+      slogan: "руский",
+      button: "руский",
+      titleAboutMe: "руский",
+      messageAboutMe: "руский",
+      titleProjects: "руский",
+      titleContact: "руский",
+    },
+  ],
 };
 
 export default dictionary;
