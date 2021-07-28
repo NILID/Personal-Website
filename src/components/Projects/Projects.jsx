f7293bba36ff35201ca3ee9ee522cd22a6661f7c
Flavio <flavioandrade0201@gmail.com>
2021-07-28 14:10:32 +0300

    Projects Component translation added

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,57 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Projects.scss";
 
 import { Project, Container, Title, Card, Images } from "./ProjectsElements";
 
+import dictionary from "../Dictionary/dictionary";
+import { LanguageContext } from "../../App";
+
 function Projects() {
+  const [language] = useContext(LanguageContext);
+
   return (
     <>
-      <Project>
-        <Container>
-          <Title>
-            <h1>Projects</h1>
-          </Title>
-          <Card>
-            {/* {data.projects.map((project, index) => (
-                <Images
-                  key={index}
-                  heading={project.title}
-                  paragraph={project.para}
-                  imgUrl={project.imageSrc}
-                  projectLink={project.url}
-                ></Images>
-              ))} */}
-          </Card>
-        </Container>
-      </Project>
+      {dictionary[language].map(({ titleProjects }) => (
+        <Project>
+          <Container>
+            <Title>
+              <h1>{titleProjects}</h1>
+            </Title>
+            <Card></Card>
+          </Container>
+        </Project>
+      ))}
     </>
-
-    // <div class="project-container" id="projects">
-    //   <div className="title">
-    //     <h1>PROJECTS</h1>
-    //   </div>
-    //   <div className="project-one item">
-    //     <h2>Face Recognition</h2>
-    //     <button className="button button1">VIEW PROJECT</button>
-    //   </div>
-    //   <div className="project-two item">
-    //     <h2>Face Recognition</h2>
-    //     <button className="button button1">VIEW PROJECT</button>
-    //   </div>
-    //   <div className="project-three item">
-    //     <h2>Face Recognition</h2>
-    //     <button className="button button1">VIEW PROJECT</button>
-    //   </div>
-    // </div>
-    // {data.projects.map((project, index) => (
-    //   <Images
-    //     key={index}
-    //     heading={project.title}
-    //     paragraph={project.para}
-    //     imgUrl={project.imageSrc}
-    //     projectLink={project.url}
-    //   ></Images>
-    // ))}
   );
 }
 
