233d8f0e135045b8d87fcf785f2fcfe50cdfd035
Flavio <flavioandrade0201@gmail.com>
2021-07-30 03:50:23 +0300

    Email form

--- src/components/Contact/Email.jsx
+++ src/components/Contact/Email.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import { Form } from "./EmailElements";
+
 function Email() {
-  return <div></div>;
+  return (
+    <>
+      <Form>
+        <label htmlFor="name"></label>
+        <input type="text" />
+        <label htmlFor=""></label>
+        <input type="email" />
+      </Form>
+    </>
+  );
 }
 
 export default Email;
