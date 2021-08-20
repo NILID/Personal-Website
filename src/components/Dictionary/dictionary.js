4a0ee9fef0ef661cb293a6a6ae52851d3b6dc146
Flavio <flavioandrade0201@gmail.com>
2021-08-20 08:20:55 +0300

    Translation changes

--- src/components/Dictionary/dictionary.js
+++ src/components/Dictionary/dictionary.js
@@ -6,7 +6,7 @@ const dictionary = {
       contact: "contact",
       sloganFirstPart: "Think the Design,",
       sloganSecondPart: "and I design the code.",
-      intro: "Beautiful designed websites using ReactJS. ",
+      intro: "Beautifully designed websites using ReactJS. ",
       introSecondPart: "Coding since 2016 to provide you the best result.",
       span: "Design",
       button: "Learn more",
@@ -26,41 +26,67 @@ const dictionary = {
       about: "sobre",
       project: "projetos",
       contact: "contato",
-      slogan: "Pense no Design e eu vou desenhar teu código",
+      sloganFirstPart: "Pense no Desgin,",
+      sloganSecondPart: "E eu farei por você!",
+      intro: "Websites incríveis com ReactJS.",
+      introSecondPart:
+        "Programanod desde 2016 para entregar sempre o melhor resultado.",
+      span: "Design",
       button: "Saiba mais",
       titleAboutMe: "Sobre",
       messageAboutMe:
-        "Olá, meu nome é Flávio e atualmente sou Team Manager da empresa Olymp Trade Forex e desenvolvedor web. Atualmente, estou cursando meu último bacharelado em Engenharia de Software na Universidade Federal da Sibéria. Nos últimos 6 anos, a programação desenvolveu uma paixão minha, especificamente, na área de desenvolvimento web e blockchain. Eu dediquei meu tempo para ganhar proficiência em desenvolvimento para me aprimorar como desenvolvedor e solucionador de problemas. Sou capaz de criar sites limpos e funcionais e entregar produtos aos meus clientes com confiança, sabendo que eles são de alta qualidade.",
+        " Olá, meu nome é Flávio e atualmente sou Team Manager da empresa Olymp Trade Forex e desenvolvedor web. Atualmente, estou cursando meu último bacharelado em Engenharia de Software na Universidade Federal da Sibéria. ",
+      messageAboutMeSecondParagraph:
+        " Nos últimos 6 anos, a programação desenvolveu uma paixão minha, especificamente, na área de desenvolvimento web e blockchain. ",
+      messageAboutMeThirdParagraph:
+        " Eu dediquei meu tempo para ganhar proficiência em desenvolvimento para me aprimorar como desenvolvedor e solucionador de problemas. Sou capaz de criar sites limpos e funcionais e entregar produtos aos meus clientes com confiança, sabendo que eles são de alta qualidade. ",
       titleProjects: "Projetos",
       titleContact: "Contatos",
     },
   ],
   jp: [
     {
-      about: "約",
-      project: "事業",
+      about: "私について",
+      project: "プロジェクト",
       contact: "コンタクト",
-      slogan: "デザインを考えて、私はコードをデザインします",
-      button: "もっと知る",
-      titleAboutMe: "コンタクト",
+      sloganFirstPart: "デザインを考える,",
+      sloganSecondPart: "そして私はコードを設計します。",
+      intro: "ReactJSを使用して美しくデザインされたウェブサイト",
+      introSecondPart: "最高の結果を提供するための2016年以降のコーディング。",
+      span: "Lorem Ipsum",
+      button: "もっと",
+      titleAboutMe: "だいたい",
       messageAboutMe:
-        "こんにちは、私の名前はFlávioです。現在、Olymp Trade Forex会社のチームマネージャーであり、Web開発者です。 現在、私はシベリア連邦大学でソフトウェア工学の最後の学士号を取得しています。 過去6年間で、コーディングは、特にWeb開発とブロックチェーンの分野で私の情熱を育ててきました。 私は、開発者および問題解決者としての自分自身を向上させるために、開発に習熟することに時間を費やしてきました。 クリーンで機能的なウェブサイトを作成し、高品質であることを知って自信を持ってクライアントに製品を届けることができます.",
+        "こんにちは。私の名前はFlávioです。現在、Olymp TradeForexのチームマネージャーおよびWeb開発者です。 私は現在、シベリア連邦大学でソフトウェア工学の最後の学士号を勉強しています。",
+      messageAboutMeSecondParagraph:
+        "過去6年間で、プログラミングは、特にWebおよびブロックチェーン開発の分野で私の情熱を育ててきました。",
+      messageAboutMeThirdParagraph:
+        "私は時間をかけて開発能力を身に付け、開発者および問題解決者としての自分自身を向上させてきました。 クリーンで機能的なウェブサイトを作成し、顧客が高品質であることを確信して、自信を持って製品を顧客に届けることができます。",
       titleProjects: "プロジェクト",
       titleContact: "コンタクト",
     },
   ],
   ru: [
     {
-      about: "руский",
-      project: "руский",
-      contact: "руский",
-      slogan: "Подумайте о дизайне, а я создаю код",
-      button: "Узнайте",
-      titleAboutMe: "Обо мне",
+      about: "обо мне",
+      project: "проекты",
+      contact: "контакт",
+      sloganFirstPart: "Подумайте о дизайне,",
+      sloganSecondPart: "и я разрабатываю код.",
+      intro: "Красиво оформленные веб-сайты с использованием ReactJS.",
+      introSecondPart:
+        "Программирую с 2016 года, чтобы предоставить вам лучший результат.",
+      span: "Lorem Ipsum",
+      button: "Узнать больше",
+      titleAboutMe: "Oбо мне",
       messageAboutMe:
-        "Привет, меня зовут Флавио, и я в настоящее время являюсь менеджером команды форекс-компании Olymp Trade и веб-разработчиком. В настоящее время я получаю последнюю степень бакалавра программной инженерии в Сибирском федеральном университете. За последние 6 лет программирование стало моей страстью, особенно в области веб-разработки и блокчейна. Я посвятил свое время приобретению навыков разработки, чтобы улучшить себя как разработчик и решатель проблем. Я умею создавать чистые и функциональные веб-сайты и уверенно доставлять продукты своим клиентам, зная, что они высокого качества.",
-      titleProjects: "Проекты",
-      titleContact: "Контакт",
+        " Привет, меня зовут Флавио, и я в настоящее время являюсь менеджером команды в Olymp Trade Forex и веб-разработчиком. В настоящее время я учусь на последней степени бакалавра программной инженерии в Сибирском федеральном университете. ",
+      messageAboutMeSecondParagraph:
+        " За последние 6 лет программирование стало моей страстью, особенно в области разработки веб-технологий и блокчейнов. ",
+      messageAboutMeThirdParagraph:
+        " Я потратил время на то, чтобы приобрести навыки разработки, чтобы улучшить себя как разработчик и решатель проблем. Я могу создавать чистые, функциональные веб-сайты и с уверенностью доставлять продукты своим клиентам, зная, что они имеют высокое качество. ",
+      titleProjects: "проекты",
+      titleContact: "контакт",
     },
   ],
 };
